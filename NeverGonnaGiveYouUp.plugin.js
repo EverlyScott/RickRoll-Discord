889e247f1933833dfa1a43ea59b55a605680f9aa
ScribbleNerd <benjibear2007@gmail.com>
2021-04-01 14:53:09 -0500

    made the app go back to normal once the video ends

--- NeverGonnaGiveYouUp.plugin.js
+++ NeverGonnaGiveYouUp.plugin.js
@@ -24,8 +24,12 @@ module.exports = class {
     document.getElementsByClassName('app-2rEoOp')[0].style.display = 'none'
     var rickrollspan = document.createElement('span')
     rickrollspan.id = 'rickroll'
-    rickrollspan.innerHTML = '<video style="width:100%;height:calc(100vh - 3em);" src="https://cdn.discordapp.com/attachments/714677532125036558/827088003670343690/Rick_Astley_Never_Gonna_Give_You_Up-150K.mp4" controls autoplay></video><br /><div style="text-align:center;"><a href="#" onclick="document.getElementById(\'rickroll\').remove();document.getElementsByClassName(\'app-2rEoOp\')[0].style.display = \'flex\'">Take me back to Discord!</a></div>'
+    rickrollspan.innerHTML = '<video id="rickroll-video" style="width:100%;height:calc(100vh - 3em);" src="https://cdn.discordapp.com/attachments/714677532125036558/827088003670343690/Rick_Astley_Never_Gonna_Give_You_Up-150K.mp4" controls autoplay></video><br /><div style="text-align:center;"><a href="#" onclick="document.getElementById(\'rickroll\').remove();document.getElementsByClassName(\'app-2rEoOp\')[0].style.display = \'flex\'">Take me back to Discord!</a></div>'
     document.getElementsByClassName('app-1q1i1E')[0].appendChild(rickrollspan)
+
+    var rickrollscript = document.createElement('script')
+    rickrollscript.innerText = `document.getElementById('rickroll-video').onended = () => { document.getElementById('rickroll').remove(); document.getElementsByClassName('app-2rEoOp')[0].style.display = 'flex' }`
+    document.getElementsByClassName('app-1q1i1E')[0].appendChild(rickrollscript)
   }
 
   stop() {
