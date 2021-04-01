0c55146177fe4ceff531c1311975f7c9ea20f2f8
ScribbleNerd <benjibear2007@gmail.com>
2021-04-01 14:48:20 -0500

    made it look better

--- NeverGonnaGiveYouUp.plugin.js
+++ NeverGonnaGiveYouUp.plugin.js
@@ -24,7 +24,12 @@ module.exports = class {
     document.getElementsByClassName('app-2rEoOp')[0].style.display = 'none'
     var rickrollspan = document.createElement('span')
     rickrollspan.id = 'rickroll'
-    rickrollspan.innerHTML = '<video src="https://cdn.discordapp.com/attachments/714677532125036558/827088003670343690/Rick_Astley_Never_Gonna_Give_You_Up-150K.mp4" controls autoplay></video><br /><a href="#" onclick="document.getElementById(\'rickroll\').remove();document.getElementsByClassName(\'app-2rEoOp\')[0].style.display = \'flex\'">Take me back to Discord!</a>'
+    rickrollspan.innerHTML = '<video style="width:100%;height:calc(100vh - 3em);" src="https://cdn.discordapp.com/attachments/714677532125036558/827088003670343690/Rick_Astley_Never_Gonna_Give_You_Up-150K.mp4" controls autoplay></video><br /><div style="text-align:center;"><a href="#" onclick="document.getElementById(\'rickroll\').remove();document.getElementsByClassName(\'app-2rEoOp\')[0].style.display = \'flex\'">Take me back to Discord!</a></div>'
     document.getElementsByClassName('app-1q1i1E')[0].appendChild(rickrollspan)
+  }
+
+  stop() {
+    document.getElementById('rickroll')?.remove()
+    document.getElementsByClassName('app-2rEoOp')[0].style.display = 'flex'
   }
 }
